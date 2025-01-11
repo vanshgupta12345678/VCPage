62b4dbefe9142fdcbc605f5e01ab0309cd5c00ce
vanshgupta12345678 <vanshgupta93560@gmail.com>
2025-01-11 15:24:34 +0530

    change

--- frontendpage/src/App.js
+++ frontendpage/src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
-
+import Cards from './Components/Cards.js'
 function App() {
   return (
     <>    <Navbar/> 
 < Sidebar/>
-   
+   <Cards/>
   
       
     </>
---
+++ frontendpage/src/Components/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Card.css'
+const Cards= () => {
+    return (
+        <div className='flex'>
+           <div className='Card1'>
+
+           </div>
+           <div className='Card2'>
+
+           </div>
+           <div className='Card3'>
+
+           </div>
+        </div>
+    );
+};
+
+export default Cards;
