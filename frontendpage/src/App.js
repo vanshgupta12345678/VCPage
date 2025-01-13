172b34d5e327c5f1a436a5b03d50fb58bf101301
Chetan Chugh <chetanchughh@gmail.com>
2025-01-13 15:12:44 +0530

    Update App.js

--- frontendpage/src/App.js
+++ frontendpage/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
 import Cards from './Components/Cards.js'
-// import Card2 from './Components/Card2.js'
+import Card2 from './Components/Card2.js'
 function App() {
   return (
     <>    <Navbar/> 
 < Sidebar/>
    <Cards/>
-   {/* <Card2/> */}
+   <Card2/>
   
       
     </>
