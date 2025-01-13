eafc486cd663690746fd83c316b81647910c3c9b
vanshGupta <Vanshgupta93560@gmail.com>
2025-01-13 16:08:34 +0530

    vhange

--- frontendpage/src/Components/Card2.js
+++ frontendpage/src/Components/Card2.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 import './Card2.css';
-
+import { FaCalendarAlt } from "react-icons/fa";
 const Card2 = () => {
     return (
         <>
@@ -10,13 +10,13 @@ const Card2 = () => {
                     {/* Corrected Table Structure */}
                     <table>
                         <thead>
-                            <tr >
+                            <tr>
                                 <th style={{ fontSize: '22px' }}>Employee List</th>
                             </tr>
                         </thead>
-                        <tbody >
-                            <tr >
-                                <td >Name</td>
+                        <tbody>
+                            <tr>
+                                <td>Name</td>
                                 <td style={{ textAlign: 'center' }}>Employee ID</td>
                                 <td>Profile</td>
                             </tr>
@@ -35,13 +35,49 @@ const Card2 = () => {
                                 <td>Vansh</td>
                                 <td>Vansh</td>
                             </tr>
-
+                            <tr>
+                                <td style={{ textAlign: 'center' }} colSpan="3" >
+                                    View All Employees
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
 
                 <div className='box2'>
-                    vanshgyuaoaggu
+                    <table>
+                        <thead>
+                            <tr style={{ display: 'flex' }}>
+                                <th style={{ fontSize: '22px' }}>Up Coming Holidays</th>
+                                <td style={{ textAlign: 'cent', marginLeft: '20px' }} colSpan="3" >
+                                    View All
+                                </td>
+                            </tr>
+                        </thead>
+                    </table>
+                    <div className='date' style={{ display: 'flex' }}>
+                        <div style={{ color: 'white', fontSize: '30px', padding: '10px' }}>
+                            <FaCalendarAlt />
+                        </div>
+                        <div style={{ marginTop: '10px',color:'white' }}>
+                            Diwali<br/>Mon 20 November 2024
+                        </div>
+                    </div>
+                    <div className='date' style={{ display: 'flex' }}>
+                        <div style={{ color: 'white', fontSize: '30px', padding: '10px' }}>
+                            <FaCalendarAlt />
+                        </div>
+                        <div style={{ marginTop: '10px',color:'white' }}>
+                            Diwali<br/>Mon 20 November 2024
+                        </div>
+                    </div><div className='date' style={{ display: 'flex' }}>
+                        <div style={{ color: 'white', fontSize: '30px', padding: '10px' }}>
+                            <FaCalendarAlt />
+                        </div>
+                        <div style={{ marginTop: '10px',color:'white' }}>
+                            Diwali<br/>Mon 20 November 2024
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
