ed463e3637e43e142be4eb8b5ec6c9194cc02041
vanshGupta <Vanshgupta93560@gmail.com>
2025-01-13 15:01:00 +0530

    Employee list and holidays

--- frontendpage/src/App.js
+++ frontendpage/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
 import Cards from './Components/Cards.js'
+import Card2 from './Components/Card2.js'
 // import Card2 from './Components/Card2.js'
 function App() {
   return (
-    <>    <Navbar/> 
-< Sidebar/>
-   <Cards/>
-   {/* <Card2/> */}
-  
-      
+    <>    <Navbar />
+      < Sidebar />
+      <Cards />
+      <Card2 />
+      {/* <Card2/> */}
+
+
     </>
   );
 }
