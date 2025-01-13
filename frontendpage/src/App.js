0026f06c513efd099aec9e3894a4a6b327096edf
vanshGupta <Vanshgupta93560@gmail.com>
2025-01-13 17:46:41 +0530

    chang

--- frontendpage/src/App.js
+++ frontendpage/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
 import Cards from './Components/Cards.js'
-import Card2 from './Components/Card2.js'
+import Card2 from './Components/Card2.js';
+import Card3 from './Components/Card3.js'
+import Card4 from './Components/Card4.js';
 // import Card2 from './Components/Card2.js'
 function App() {
   return (
@@ -12,8 +14,9 @@ function App() {
       <Cards />
       <Card2 />
       {/* <Card2/> */}
+      <Card3/>
 
-
+{/* <Card4/> */}
     </>
   );
 }
---
+++ frontendpage/src/Components/Card3.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './Card3.css';
+
+const Card3 = () => {
+    return (
+        <>
+        <div>
+        <div style={{display:'flex'}}>
+                <div style={{border:'1px  solid black',width:'667px',height:'313px',marginLeft:'300px'}}>
+
+                </div>
+              
+            </div>
+            <div style={{border:'1px  solid black',width:'667px',height:'460px',marginLeft:'300px',marginTop:'30px'}}>
+
+</div>
+  
+        </div>
+        <div style={{border:'1px  solid black',width:'435px',height:'806px',marginLeft:'986px',marginTop:'-808px'}}>
+
+</div>
+        </>
+    );
+}
+
+export default Card3;
