23aabbb19dc188ad0eb97dffc4a21f29c0d21fbb
Chetan Chugh <chetanchughh@gmail.com>
2025-01-13 15:15:25 +0530

    Create Card2.js

---
+++ frontendpage/src/Components/Card2.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Card.css';
+import './Card2.css';
+
+const Card2 = () => {
+    return (
+        <>
+            <div className="card-container">
+                <div className='box'>
+                    {/* Corrected Table Structure */}
+                    <table>
+                        <thead>
+                            <tr >
+                                <th style={{ fontSize: '22px' }}>Employee List</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            <tr >
+                                <td >Name</td>
+                                <td style={{ textAlign: 'center' }}>Employee ID</td>
+                                <td>Profile</td>
+                            </tr>
+                            <tr>
+                                <td>Vansh</td>
+                                <td>Vansh</td>
+                                <td style={{ textAlign: 'right', paddingLeft: '4rem' }}>Vansh</td>
+                            </tr>
+                            <tr>
+                                <td>Vansh</td>
+                                <td>Vansh</td>
+                                <td>Vansh</td>
+                            </tr>
+                            <tr>
+                                <td>Vansh</td>
+                                <td>Vansh</td>
+                                <td>Vansh</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className='box2'>
+                    vanshgyuaoaggu
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Card2;
