84290d4a2f158ef3fc4d3eeacae3a4d331df8fc0
vanshGupta <Vanshgupta93560@gmail.com>
2025-01-14 11:39:18 +0530

    chang

--- frontendpage/src/App.js
+++ frontendpage/src/App.js
@@ -5,7 +5,7 @@ import Navbar from './Components/Navbar';
 import Cards from './Components/Cards.js'
 import Card2 from './Components/Card2.js';
 import Card3 from './Components/Card3.js'
-import Card4 from './Components/Card4.js';
+// import Card4 from './Components/Card4.js';
 // import Card2 from './Components/Card2.js'
 function App() {
   return (
--- frontendpage/src/Components/Card3.js
+++ frontendpage/src/Components/Card3.js
@@ -1,25 +1,36 @@
 import React from "react";
 import './Card3.css';
+import logo from '../Components/icon.jpg';
 
 const Card3 = () => {
     return (
-        <>
-        <div>
-        <div style={{display:'flex'}}>
-                <div style={{border:'1px  solid black',width:'667px',height:'313px',marginLeft:'300px'}}>
-
+        <div className="card-container">
+            <div className="Crad3">
+                <div>
+                    DIv1
                 </div>
-              
             </div>
-            <div style={{border:'1px  solid black',width:'667px',height:'460px',marginLeft:'300px',marginTop:'30px'}}>
 
-</div>
-  
+            <div className="card4">
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <h3 style={{ paddingTop: '5px', marginLeft: '10px' }}>New Joiner Candidate</h3>
+                    <p style={{ marginRight: '10px', color: '#006EBE', borderBottom: '1px solid black' }}>
+                        View All
+                    </p>
+                </div>
+                {[...Array(3)].map((_, index) => (
+                    <div className="color" key={index} style={{ marginTop: index > 0 ? '20px' : '0' }}>
+                        <div className='logo'>
+                            <img src={logo} alt="Logo" />
+                            <div style={{ marginLeft: '10px', display: 'flex', flexDirection: 'column' }}>
+                                <p style={{marginTop:'20px'}}>DiannaRusael<br />ATSID1234</p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-        <div style={{border:'1px  solid black',width:'435px',height:'806px',marginLeft:'986px',marginTop:'-808px'}}>
-
-</div>
-        </>
     );
 }
 
