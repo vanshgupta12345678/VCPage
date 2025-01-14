7ec5e152f1f117d9c9c5d461c8771b2938e50bdc
vanshGupta <Vanshgupta93560@gmail.com>
2025-01-14 15:07:34 +0530

    Change

---
+++ frontendpage/src/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZLDM6r91KLDszk_fZ3ZJC8VpgL1PmRi4",
+  authDomain: "react-de035.firebaseapp.com",
+  projectId: "react-de035",
+  storageBucket: "react-de035.firebasestorage.app",
+  messagingSenderId: "777977315790",
+  appId: "1:777977315790:web:2d0cc9d6da4fa393c22b9f"
+};
+
+const app = initializeApp(firebaseConfig);
